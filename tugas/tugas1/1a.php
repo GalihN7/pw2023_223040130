243b4b2ffb1e8b4e991c522ba76de93f8d3e8385
GalihN7 <124852405+GalihN7@users.noreply.github.com>
2023-02-22 20:24:37 +0700

    Add files via upload

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,13 @@
+<?php 
+   $benda1 = 'Topi';
+   $benda2 = 'Bundar';
+   ?>
+   
+   <!DOCTYPE html>
+<html>
+<body>
+   <?php 
+    echo "<h2><i>\"$benda1 saya $benda2, $benda2 $benda1 saya\"</i></h2>";
+    ?>
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <?php 
+        $x = "30"
+        ?>
+        <p> Aku adalah angka <span> <?php echo "<b>$x</b>" ?> </span> </p>
+        <p> Jika aku dikali 5, maka aku sekarang menjadi <span> <b><?php echo $x * 5; ?> </span></b> </p>
+        <p> Jika aku dibagi 2, maka aku sekarang menjadi <span> <b><?php echo $x / 2; ?> </span></b> </p>
+        <p> Jika aku ditambah 75, maka aku sekarang menjadi <span> <b><?php echo $x + 75; ?> </span></b> </p>
+        <p> Jika aku dikurang 20, maka aku sekarang menjadi <span> <b><?php echo $x - 20; ?> </span></b> </p>
+    </body>
+</html>
+
